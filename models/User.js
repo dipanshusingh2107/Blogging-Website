a84f215f0c1bb930238b3657a45ee6566fd81084
dipanshusingh2107 <dipanshusingh2107@gmail.com>
2021-05-17 21:34:58 +0530

    fixing stuffs

--- models/User.js
+++ models/User.js
@@ -5,12 +5,12 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const userSchema = new mongoose.Schema ({
     name: String,
     email: {type:String , unique:true,sparse: true},
-    username: {type:String , 
+    username: {type:String} , 
       index: {
         unique: true,
         partialFilterExpression: { email: { $type: 'string' } },
-      }
       },
+    default : null,
     password: String,
     googleId: String
   });
